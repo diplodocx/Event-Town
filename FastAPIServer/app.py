67d578487029b77407bf82c26071c9d8f3c2bf01
diplodocx <zabarinternational@gmail.com>
2023-05-12 23:40:02 +0300

    auth with poetry bug

--- FastAPIServer/app.py
+++ FastAPIServer/app.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI, Depends
+
+from src.auth.auth import auth_backend
+from src.auth.routers import fastapi_users
+from src.auth.schemas import UserRead, UserCreate
+
 # from starlette.responses import JSONResponse
 #
 # from src.auth.auth import auth_backend
@@ -8,19 +13,19 @@ from fastapi import FastAPI, Depends
 # from src.manager.routers import manager
 # from src.reports.routers import reports
 
-app = FastAPI(title="kanban app")
+app = FastAPI(title="eventtown app")
 
 # current_user = fastapi_users.current_user()
 #
 # app.include_router(manager)
 # app.include_router(reports)
-# app.include_router(
-#     fastapi_users.get_auth_router(auth_backend),
-#     prefix="/auth",
-#     tags=["auth"],
-# )
-# app.include_router(
-#     fastapi_users.get_register_router(UserRead, UserCreate),
-#     prefix="/auth",
-#     tags=["auth"],
-# )
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
--- FastAPIServer/src/auth/models.py
+++ FastAPIServer/src/auth/models.py
@@ -1,4 +1,6 @@
 import sqlalchemy as db
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 metadata = db.MetaData()
 
@@ -14,3 +16,29 @@ user = db.Table(
     db.Column("is_recipient", db.Boolean, default=False, nullable=False)
 )
 
+class Base(DeclarativeBase):
+    pass
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id: Mapped[int] = mapped_column(
+        db.Integer, autoincrement=True, primary_key=True
+    )
+    email: Mapped[str] = mapped_column(
+        db.String(200), unique=True, nullable=False
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        db.String(1024), nullable=False
+    )
+    is_active: Mapped[bool] = mapped_column(
+        db.Boolean, default=True, nullable=False
+    )
+    is_superuser: Mapped[bool] = mapped_column(
+        db.Boolean, default=False, nullable=False
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        db.Boolean, default=False, nullable=False
+    )
+    is_recipient: Mapped[bool] = mapped_column(
+        db.Boolean, default=False, nullable=False
+    )
