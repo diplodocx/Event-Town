dd0b5894b50096ed58985c15100b106b33aed983
diplodocx <zabarinternational@gmail.com>
2023-05-16 01:44:25 +0300

    added rabbitmq and celery

--- FastAPIServer/src/messages/database.py
+++ FastAPIServer/src/messages/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, Session
 
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
-DATABASE_URL = f"postgresql+pyscopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 session_maker = sessionmaker(engine, expire_on_commit=False)
--- FastAPIServer/src/messages/messages.py
+++ FastAPIServer/src/messages/messages.py
@@ -4,17 +4,17 @@ from . import queries
 from celery import Celery
 from sqlalchemy.orm import Session
 import smtplib
-from config import SMTP_PASSWORD, SMTP_USER
+from config import SMTP_PASSWORD, SMTP_USER, RABBIT_USER, RABBIT_PASS
 from .database import get_session
 
-celery = Celery('tasks', broker="redis://localhost:6379")
+celery = Celery('tasks', broker=f"amqp://{RABBIT_USER}:{RABBIT_PASS}@80.90.186.118:5672")
 SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = 465
 
 
 def make_text_body(event):
     event_data = event.dict()
-    return
+    return ""
 
 
 def make_template(event, session: Session):
@@ -29,7 +29,8 @@ def make_template(event, session: Session):
 
 @celery.task()
 def send_report(event, session: Session = next(get_session())):
-    msg = make_template(session, event)
-    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
-        server.login(SMTP_USER, SMTP_PASSWORD)
-        server.send_message(msg)
+    print("made")
+    # msg = make_template(event, session)
+    # with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
+    #     server.login(SMTP_USER, SMTP_PASSWORD)
+    #     server.send_message(msg)
